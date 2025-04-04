e28ec7bc4c8b0cda0775e6ed696556ae6bc8b4a8
aynya <1191358246@qq.com>
2025-04-04 15:27:05 +0800

    ✨ feat: 修改了令牌过期的时间

--- server/config/indexConfig.js
+++ server/config/indexConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     jwtSecretKey: process.env.JWT_SECRET_KEY || 'your-default-secret-key',
     jwtRefreshSecretKey: process.env.JWT_REFRESH_SECRET_KEY || 'your-default-refresh-secret-key',
-    jwtAccessTokenExpiry: '3s', // 访问令牌有效期
-    jwtRefreshTokenExpiry: '60s', // 刷新令牌有效期
+    jwtAccessTokenExpiry: '3600s', // 访问令牌有效期 一小时
+    jwtRefreshTokenExpiry: '86400s', // 刷新令牌有效期 一天
 };
